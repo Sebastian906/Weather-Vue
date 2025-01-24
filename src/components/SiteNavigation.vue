cc94e95ccecf4917a68a04eef5de5a6f41082812
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-01-23 22:48:13 -0500

    Adición y configuración de nota Modal para la aplicación

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -8,14 +8,52 @@
                 </div>
             </RouterLink>
             <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal"></i>
                 <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
             </div>
+            <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+                <div class="text-black">
+                    <h1 class="text-2xl mb-1">Información:</h1>
+                    <p class="mb-4">
+                        The Local Weather le permite ver el clima actual y la previsión del tiempo para los próximos 7 días.
+                    </p>
+                    <h2 class="text-2xl">Como funciona:</h2>
+                    <ol class="list-decimal list-inside mb-4">
+                        <li>
+                            Busca tu ciudad escrbiendo su nombre dentro de la barra de búsqueda.
+                        </li>
+                        <li>
+                            Selecciona tu ciudad de la lista de resultados. 
+                            Esto lo redireccionará al clima actual de la ciudad.
+                        </li>
+                        <li>
+                            Rastrea la ciudad al darle click en el ícono de '+' en 
+                            la esquina superior derecha. Esto guardar'a la ciudad para 
+                            revisarla más tarde en la página principal.
+                        </li>
+                    </ol>
+
+                    <h2 class="text-2xl">Remover una ciudad</h2>
+                    <p>
+                        Si ya no desea rastrear una ciudad, simplemente seleccione 
+                        la ciudad en la página principal. En el fondo de la página, 
+                        habrá una opción para eliminar la ciudad.
+                    </p>
+                </div>
+            </BaseModal>
         </nav>
     </header>
 </template>
 
 <script setup>
 import { RouterLink } from "vue-router";
+import BaseModal from "./BaseModal.vue";
+import { ref } from "vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+    modalActive.value = !modalActive.value;
+}
+
 </script>
 
